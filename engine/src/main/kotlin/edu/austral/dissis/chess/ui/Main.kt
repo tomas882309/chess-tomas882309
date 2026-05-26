8914032f5fab4be3437912e536d44329cebd1ed3
Javier Isoldi <javierisoldi@gmail.com>
2026-05-26 16:01:09 -0300

    Main added with basic setup for JavaFx

---
+++ engine/src/main/kotlin/edu/austral/dissis/chess/ui/Main.kt
@@ -0,0 +1,32 @@
+package edu.austral.dissis.chess.ui
+
+import edu.austral.dissis.chess.gui.CachedImageResolver
+import edu.austral.dissis.chess.gui.DefaultImageResolver
+import edu.austral.dissis.chess.gui.GameViewFactory
+import javafx.application.Application
+import javafx.application.Application.launch
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+
+fun main() {
+    launch(ChessGameApplication::class.java)
+}
+
+class ChessGameApplication : Application() {
+    private val imageResolver = CachedImageResolver(DefaultImageResolver())
+
+    companion object {
+        const val GameTitle = "Chess"
+    }
+
+    override fun start(primaryStage: Stage) {
+        primaryStage.title = GameTitle
+
+        val root = GameViewFactory.create(imageResolver)
+
+        primaryStage.scene = Scene(root.pane())
+
+        primaryStage.show()
+    }
+}
